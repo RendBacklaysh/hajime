4a3030acfbb68b0105bd14f406c0f0427af01191
RendBacklaysh <hai.stuw@gmail.com>
2018-09-15 13:56:01 +0700

    Hajime
    Hajime

---
+++ Hajime/Controllers/TESTController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hajime.Controllers
+{
+    public class TESTController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TEST()
+        {
+            return View();
+        }
+
+    }
+}
